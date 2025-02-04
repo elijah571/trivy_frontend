e9ba7bd51297d860812cf471ac3823ac8df314b8
elijah571 <elijahfx43@gmail.com>
2025-02-03 16:15:33 -0800

    api fixed

--- src/redux/api/teacherSlice.js
+++ src/redux/api/teacherSlice.js
@@ -5,48 +5,55 @@ export const teacherApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     loginTeacher: builder.mutation({
       query: (data) => ({
-        url: `${TEACHERS_URL}/login`, // Login route for teacher
+        url: `${TEACHERS_URL}/login`,  // Use template literal correctly
         method: "POST",
         body: data,
+        credentials: "include",
       }),
     }),
     registerTeacher: builder.mutation({
       query: (data) => ({
-        url: `${TEACHERS_URL}/create`, // Create teacher route
+        url: `${TEACHERS_URL}/create`,  // Use template literal correctly
         method: "POST",
         body: data,
+        credentials: "include",
       }),
     }),
     logoutTeacher: builder.mutation({
       query: () => ({
-        url: `${TEACHERS_URL}/logout`, // Logout teacher route
+        url: `${TEACHERS_URL}/logout`,  // Use template literal correctly
         method: "POST",
+        credentials: "include",
       }),
     }),
     getTeachers: builder.query({
       query: () => ({
-        url: TEACHERS_URL, // Get all teachers route (admin access)
+        url: `${TEACHERS_URL}`,  // Use template literal correctly
+        credentials: "include",
       }),
       providesTags: ["Teacher"],
       keepUnusedDataFor: 5,
     }),
     deleteTeacher: builder.mutation({
       query: (teacherId) => ({
-        url: `${TEACHERS_URL}/${teacherId}`, // Delete specific teacher
+        url: `${TEACHERS_URL}/${teacherId}`,  // Use template literal correctly
         method: "DELETE",
+        credentials: "include",
       }),
     }),
     getTeacherDetails: builder.query({
       query: (id) => ({
-        url: `${TEACHERS_URL}/${id}`, // Get specific teacher details
+        url: `${TEACHERS_URL}/${id}`,  // Use template literal correctly
+        credentials: "include",
       }),
       keepUnusedDataFor: 5,
     }),
     updateTeacher: builder.mutation({
       query: (data) => ({
-        url: `${TEACHERS_URL}/${data.teacherId}`, // Update specific teacher info
+        url: `${TEACHERS_URL}/${data.teacherId}`,  // Use template literal correctly
         method: "PUT",
         body: data,
+        credentials: "include",
       }),
       invalidatesTags: ["Teacher"],
     }),
